0a7ed9f0b9b27aad0c5a3f59e711b6cdf095840f
n3mar <eikemartensen@gmail.com>
2026-06-13 09:56:37 -0400

    Task2

---
+++ emotion_detection.py
@@ -0,0 +1,11 @@
+"""This module detects emotions for text inputs"""
+import requests
+
+def emotion_detector(text_to_analyze):
+    """This function returns emotions for input text"""
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header_json = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = { "raw_document": { "text": text_to_analyze } }
+    response = requests.post(url, json = input_json, headers = header_json)
+    return response.text
+    
